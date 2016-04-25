6f2374ca4a014c42b159f326eca6a45bdf32f218
Mauricio Andrades <mod.and.op@gmail.com>
2016-04-25 08:31:18 -0700

    all sansmono

--- theme-extension/devtools.js
+++ theme-extension/devtools.js
@@ -1,10 +1,22 @@
 var xhr = new XMLHttpRequest(),
-	stylesheet = 'stable.css';
+	local_inspectorStylesheet = 'inspector.css',
+	local_inspectorCommon = 'inspectorCommon.css',
+	local_stylesheet = 'stable.css';
 
 if (/Chrome\/(\d\d)/.exec(navigator.userAgent)[1] > 48) {
-	stylesheet = 'canary.css';
+  local_stylesheet = 'canary.css';
 }
 
-xhr.open("GET", "/" + stylesheet, false);
+xhr.open("GET", "/" + local_inspectorCommon, false);
 xhr.send();
 chrome.devtools.panels.applyStyleSheet(xhr.responseText);
+
+xhr.open("GET", "/" + local_inspectorStylesheet, false);
+xhr.send();
+chrome.devtools.panels.applyStyleSheet(xhr.responseText);
+
+
+xhr.open("GET", "/" + local_stylesheet, false);
+xhr.send();
+chrome.devtools.panels.applyStyleSheet(xhr.responseText);
+
