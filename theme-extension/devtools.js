9ea715f440a07b20d4509fbae46af6ae7da266fa
Mauricio Andrades <mod.and.op@gmail.com>
2016-04-26 09:45:45 -0700

    merged and min all css

--- theme-extension/devtools.js
+++ theme-extension/devtools.js
@@ -1,22 +1,11 @@
 var xhr = new XMLHttpRequest(),
-	local_inspectorStylesheet = 'inspector.css',
-	local_inspectorCommon = 'inspectorCommon.css',
-	local_stylesheet = 'stable.css';
+	stylesheet = 'canary-stable.css';
 
 if (/Chrome\/(\d\d)/.exec(navigator.userAgent)[1] > 48) {
-  local_stylesheet = 'canary.css';
+  stylesheet = 'canary-stable.css';
 }
 
-xhr.open("GET", "/" + local_inspectorCommon, false);
-xhr.send();
-chrome.devtools.panels.applyStyleSheet(xhr.responseText);
-
-xhr.open("GET", "/" + local_inspectorStylesheet, false);
-xhr.send();
-chrome.devtools.panels.applyStyleSheet(xhr.responseText);
-
-
-xhr.open("GET", "/" + local_stylesheet, false);
+xhr.open("GET", "/" + stylesheet, false);
 xhr.send();
 chrome.devtools.panels.applyStyleSheet(xhr.responseText);
 
